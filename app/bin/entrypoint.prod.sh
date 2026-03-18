1bc6ec7b95499818a833a0e73667bd7b03cc1ab3
sfbdata <sfb.samuell@gmail.com>
2026-03-18 10:31:18 -0300

    fix: volume static_prod para servir assets via nginx

--- app/bin/entrypoint.prod.sh
+++ app/bin/entrypoint.prod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-
+echo "==> Copiando assets estáticos..."
+cp -rn /var/www/app/public/. /var/www/static/
 echo "==> Rodando migrations..."
 php /var/www/app/bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
-
 echo "==> Iniciando PHP-FPM..."
 exec "$@"
