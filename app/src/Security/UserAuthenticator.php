f43e042602ffd179024ec82a39b96b6b20c4633e
sfbdata <sfb.samuell@gmail.com>
2026-04-01 16:56:02 -0300

    fix(security): remove LoginThrottlingBadge não disponível no Symfony

--- app/src/Security/UserAuthenticator.php
+++ app/src/Security/UserAuthenticator.php
@@ -10,7 +10,6 @@ use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\LoginThrottlingBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -36,7 +35,6 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
             new PasswordCredentials($request->request->get('password', '')),
             [
                 new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
-                new LoginThrottlingBadge(),
             ]
         );
     }
