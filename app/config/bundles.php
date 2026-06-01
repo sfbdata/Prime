344ac5575b5e82881fc57d8516add92ed849babc
sfbdata <sfb.samuell@gmail.com>
2026-06-01 14:47:40 -0300

    chore(test): instala Foundry e DAMA para testes

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -11,4 +11,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
